92366d2bd6dc1790069e21537313cc27519b767f
SaravananJayabal <saravananmsd957@gamil.com>
2022-02-01 11:23:44 +0530

    second push in master

---
+++ src/DemoForGitBash/Demo001Test.java
@@ -0,0 +1,10 @@
+package DemoForGitBash;
+
+public class Demo001Test {
+
+	public static void main(String[] args) {
+	System.out.println("printing here first file");
+	System.out.println("printing here new file");
+	}
+
+}
--- src/DemoForGitBash/Demo002Test.java
+++ src/DemoForGitBash/Demo002Test.java
@@ -1,6 +1,6 @@
 package DemoForGitBash;
 
-public class Demo001 {
+public class Demo002Test {
 
 	public static void main(String[] args) {
 	System.out.println("printing here");
